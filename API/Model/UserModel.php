189b05dba0d0907d53281de39778c8adae26914f
JD <djasoliya6141@conestogac.on.ca>
2023-02-20 20:32:13 -0500

    login_done

--- API/Model/UserModel.php
+++ API/Model/UserModel.php
@@ -5,17 +5,25 @@ class UserModel extends Database
     /* Gets the users from the DB tables*/
     public function getUsers($limit)
     {
-        echo("limit = " . $limit);
+         
         return $this->select("SELECT * FROM user ORDER BY user_id ASC LIMIT ?", ["i", $limit]);
+        
     }
 
     public function signupUsers($username,$email,$pass)
     {
                 $history = '';
                 $shipping_address = '';
-                echo("pass = " . $pass);
+                 
                 return $this->signup("INSERT INTO `user` (`username`, `email`, `password`, `history`, `shipping_address`) VALUES ('$username', '$email','$pass','', '')");
         
     }
 
+    public function loginUser($username,$pass)
+    {
+                // echo 'username :' . $username;
+                //echo 'password :' . $pass;
+                return $this->login("SELECT * FROM `user` WHERE username = '$username' AND password='$pass'");
+        
+    }
 }
