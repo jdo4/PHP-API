eb0244e0c412b623a5f0fc5f65953b582d486bed
AJ <amaniya8692@conestogac.on.ca>
2023-02-20 19:01:14 -0500

    before_login

--- API/Controller/Api/UserController.php
+++ API/Controller/Api/UserController.php
@@ -46,6 +46,50 @@ class UserController extends BaseController
         }
     }
 
+    public function signup()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+
+                $userModel = new UserModel();
+                $intLimit = 10;
+                $username = '';
+                $email = '';
+                $pass = '';
+                echo $_POST["username"];
+                echo("pass = " . $arrQueryStringParams['username']);
+                if (isset($_POST["username"]) && $_POST["username"] && isset($_POST['email']) && $_POST['email'] && isset($_POST['password']) && $_POST['password']) {
+                    $username = $_POST['username'];
+                    $email = $_POST['email'];
+                    $pass = $_POST['password'];
+                }
+                $arrUsers = $userModel->signupUsers($username,$email,$pass);
+                echo $arrUsers;
+                $responseData = $arrUsers;
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+        // send output 
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+
     /* "/user/list/123" - get user by ID Post */
     public function getUserByID($id)
     {
--- API/inc/config.php
+++ API/inc/config.php
@@ -2,4 +2,4 @@
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_DATABASE_NAME", "rest_api_demo");
+define("DB_DATABASE_NAME", "rest_api");
